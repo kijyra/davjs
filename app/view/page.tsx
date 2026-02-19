458a840d0843486c8ded7f028e9ed8ccbe27e324
kijyra <kijyra@gmail.com>
2026-02-19 16:38:52 +0300

    fix api link

--- app/view/page.tsx
+++ app/view/page.tsx
@@ -8,7 +8,7 @@ interface PageProps {
 export default async function ViewPage({ searchParams }: PageProps) {
   const { buildingId, floorId } = await searchParams;
 
-  let endpoint = '/view/Data';
+  let endpoint = '/api/view/Data';
   const params = new URLSearchParams();
   if (buildingId) params.append('buildingId', buildingId);
   if (floorId) params.append('floorId', floorId);
